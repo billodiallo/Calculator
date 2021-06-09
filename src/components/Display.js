ad35001bea338903a89a916dc6cfcfb4b00a518c
Billo Diallo <billodiallonet@gmail.com>
2021-06-09 16:47:40 +0300

    Add Display class

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import Prototypes from 'prop-types';
+
+class Display extends React.Component {
+    render() {
+        const {result} = this.props;
+        return (
+            <div className= 'result-card'>
+                <p className='input-res'>2 * 0</p>
+                <p className="cal-res">{result}</p>
+            </div>
+        );
+    }
+}
