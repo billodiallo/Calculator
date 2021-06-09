3e0d35d28513344164cdfbdcf1c2a2e36ff516ca
Billo Diallo <billodiallonet@gmail.com>
2021-06-09 10:58:04 +0300

    Add function APP and call Display and Button

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+function App() {
+return (
+    <div className="container">
+        <div className="main-intro">
+            <div className="resul-card">
+           <Display/>
+           </div>
+           <div className="button-field">
+               <ButtonPanel/>
+           </div>
+        </div>
+    </div>
+)
+}
