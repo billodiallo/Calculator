31423ad93f994352d9d9d0a8bea3baeb5cd06049
Billo Diallo <billodiallonet@gmail.com>
2021-06-09 20:25:31 +0300

    Refactor class ButtonPanel and create buttonGroups

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -2,48 +2,19 @@ import React from 'react';
 import Button from './Button';
 
 class ButtonPanel extends React.Component {
-    render() {
-        return (
-            <div className="buton-list">
-                <div className="btn-group">
-                    {' '}
-                    <Button number="AC" color="btn-dark btn" />
-                    <Button number="+/-" color="btn-dark btn" />
-                    <Button number="%" color="btn-dark btn" />
-                    <Button number="÷" color="btn-yellow btn" />
-                    {' '}  
-                </div>
-            
-            <div className = "btn-group">
-                <Button number="7" color="btn-white btn" />
-               <Button number="8" color="btn-white btn" />
-               <Button number="9" color="btn-white btn" />
-               <Button number="X" color="btn-yellow btn" />
-            </div>
-
-            <div className = "btn-group">
-                  <Button number="4" color="btn-white btn" />
-                 <Button number="5" color="btn-white btn" />
-                <Button number="6" color="btn-white btn" />
-                <Button number="-" color="btn-yellow btn" />
-            </div>
-            <div className="btn-group">
-                <Button number="1" color="btn-white btn" />
-                <Button number="2" color="btn-white btn" />
-                <Button number="3" color="btn-white btn" />
-                <Button number="+" color="btn-yellow btn" />
-                </div>
-                
-            <div className="last-group">
-                <Button number="0" color=" zero-btn btn-last-color" />
-                <Button number="." color=" last-btn btn-last-color" />
-                <Button number="=" color=" last-btn btn-yellow " />
-                </div>
-           </div>
-
-
-        );
-    }
+    constructor(props) {
+        super(props);
+        this.props = props;
+        this.state = {
+          buttonGroups: [
+            { buttons: ['AC', '-', '%', '/'] },
+            { buttons: ['7', '8', '9', 'X'] },
+            { buttons: ['4', '5', '6', '-'] },
+            { buttons: ['1', '2', '3', '+'] },
+            { buttons: ['0', '.', '='] },
+          ],
+        };
+      }
 }
 
 export default ButtonPanel;
