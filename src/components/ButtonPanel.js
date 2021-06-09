a799a72ba65cf3d41144e5e8c0e886e04b6b2f68
Billo Diallo <billodiallonet@gmail.com>
2021-06-09 12:05:27 +0300

    Add some Buttons(AC,+/-,%,÷,colors

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,3 +1,19 @@
 import React from 'react';
 import Button from './Button';
 
+class ButtonPanel extends React.Component {
+    render() {
+        return (
+            <div className="buton-list">
+                <div className="btn-group">
+                    {' '}
+                    <Button number="AC" color="btn-dark btn" />
+                    <Button number="+/-" color="btn-dark btn" />
+                    <Button number="%" color="btn-dark btn" />
+                    <Button number="÷" color="btn-yellow btn" />  
+                </div>
+            </div>
+
+        );
+    }
+}
