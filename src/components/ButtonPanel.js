d9b1f644483c4ad3f490a5bad0b2863b7d2a20d5
Billo Diallo <billodiallonet@gmail.com>
2021-06-09 12:23:00 +0300

    Add Button 0,.,= and colors

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -13,8 +13,7 @@ class ButtonPanel extends React.Component {
                     <Button number="÷" color="btn-yellow btn" />
                     {' '}  
                 </div>
-            </div>
-
+            
             <div className = "btn-group">
                 <Button number="7" color="btn-white btn" />
                <Button number="8" color="btn-white btn" />
@@ -34,6 +33,14 @@ class ButtonPanel extends React.Component {
                 <Button number="3" color="btn-white btn" />
                 <Button number="+" color="btn-yellow btn" />
                 </div>
+                
+            <div className="last-group">
+                <Button number="0" color=" zero-btn btn-last-color" />
+                <Button number="." color=" last-btn btn-last-color" />
+                <Button number="=" color=" last-btn btn-yellow " />
+                </div>
+           </div>
+
 
         );
     }
