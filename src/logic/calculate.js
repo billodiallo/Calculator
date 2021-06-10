973cf08ce258116c55499f486638476693e9f34f
Billo Diallo <billodiallonet@gmail.com>
2021-06-10 16:09:01 +0300

    Add else condition , value to return

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,28 +1,45 @@
-import './operate'
-const calculate = (dataObjet,buttonName) => {
-    let {total,next,operation} = dataObjet;
-    const prevTotal = total;
+import operate from './operate';
 
-    
+const calculate = (dataObj, buttonName) => {
+  let { total, next, operation } = dataObj;
+  const prevTotal = total;
+
   if (['+/-', '/', 'x', '*', '+', '-', '%', '='].includes(buttonName)) {
     switch (buttonName) {
       case '=':
         total = operate(total, next, operation);
         break;
-
-        case 'AC':
+      case 'AC':
         total = '0';
         next = null;
         operation = null;
         break;
+      case '+/-':
+        total *= -1;
+        next *= -1;
+        break;
+      case '/': case 'x': case '+': case '-': case '%':
+        operation = buttonName;
+        break;
+      case '.':
+        next = +buttonName;
+        break;
+      default:
+        total = null;
+        break;
+    }
+  } 
+  else {
+    next = next.toString() + buttonName.toString();
+    next = next.toNumber();
+  }
 
-        case '+/-':
-            total *= -1;
-            next *= -1;
-            break;
+  return {
+    prevTotal,
+    total,
+    next,
+    operation,
+  };
+};
 
-        case '/': case 'x': case '+': case '-': case '%':
-            operation = buttonName;
-             break;
-
-}
+export default calculate;
