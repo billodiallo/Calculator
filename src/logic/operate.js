42ab2c64402c74ac881e01e8c7f7d447578e4a74
Billo Diallo <billodiallonet@gmail.com>
2021-06-10 15:18:57 +0300

    Add substraction operation

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -3,6 +3,11 @@ const operate = (numberone,numbertwo,operation) => {
     const firstValue = Big(numberone);
     const secondValue = Big(numbertwo);
 
+    if (operate == '-' {
+        return firstValue.minus(secondValue);
+
+    }
+
 }
 
 export default operate;
