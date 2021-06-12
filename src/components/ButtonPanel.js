c2722a9b2f247d8106b79948463e4932724e1421
Billo Diallo <billodiallonet@gmail.com>
2021-06-12 16:03:08 +0300

    ClickHandler issues

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,45 +1,55 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Button from './Button';
 
-  export default class ButtonPanel extends Component {
-    constructor(props) {
-        super(props);
-        this.props = props;
-        this.state = {
-          buttonGroups: [
-            { buttons: ['AC', '+/-', '%', '/'] },
-            { buttons: ['7', '8', '9', 'X'] },
-            { buttons: ['4', '5', '6', '-'] },
-            { buttons: ['1', '2', '3', '+'] },
-            { buttons: ['0', '.', '='] },
-          ],
-        };
-      }
+export default class ButtonPanel extends Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+    this.state = {
+      buttonGroups: [
+        { buttons: ['AC', '+/-', '%', '/'] },
+        { buttons: ['7', '8', '9', 'x'] },
+        { buttons: ['4', '5', '6', '-'] },
+        { buttons: ['1', '2', '3', '+'] },
+        { buttons: ['0', '.', '='] },
+      ],
+    };
+  }
 
-      render () {
-        let buttonsView = [];
-        const {buttonGroups} = this.state;
-        const {clickHandler} = this.props;
+  render() {
+    let buttonsView = [];
 
-        buttonsView = (
-          <div>
-            {buttonGroups.map(group => (
-              <div className="key-list" key={`${group.buttons.join('-')}`}>
-                {group.buttons.map(button => (
-                  <Button
-                    key={`btn-${button}`}
-                    buttonName={button}
-                  />
-                ))}
-              </div>
+    const { buttonGroups } = this.state;
+    const { clickHandler } = this.props;
+
+    buttonsView = (
+      <div>
+        {buttonGroups.map(group => (
+          <div className="key-list" key={`${group.buttons.join('-')}`}>
+            {group.buttons.map(button => (
+              <Button
+                key={`btn-${button}`}
+                buttonName={button}
+                handleClick={button => clickHandler(button)}
+              />
             ))}
           </div>
-        );
-        return (
-          <div>
-            {buttonsView}
-          </div>
-        );
-      }
-          
-      }
+        ))}
+      </div>
+    );
+    return (
+      <div>
+        {buttonsView}
+      </div>
+    );
+  }
+}
+
+ButtonPanel.defaultProps = {
+  clickHandler: null,
+};
+
+ButtonPanel.propTypes = {
+  clickHandler: PropTypes.func,
+};
