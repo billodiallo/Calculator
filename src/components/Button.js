3128ef954960c4887f9a99c02e3ec4a5291c7fb8
Billo Diallo <billodiallonet@gmail.com>
2021-06-12 11:14:00 +0300

    Declare handleClick ,btnStyle

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import operate from '../logic/operate';
 
 export default class Button extends Component {
   constructor(props) {
@@ -8,7 +9,16 @@ export default class Button extends Component {
   }
 
   render() {
-    const { buttonName } = this.props;
+    const { buttonName, handleClick } = this.props;
+    let btnStyle = 'normal-button';
+    if (['/','-','+','=','+/-','%'].includes(buttonName)){
+      btnStyle = 'operation-button';
+    }
+
+    return (
+      <button type="button"
+      onClick={() => handleClick(buttonName)}
+    )
 
     return (
       <p className="button">{buttonName}</p>
