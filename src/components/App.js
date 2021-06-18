4754356603a8ea8fc3779a4dc33dcd845fffe912
Billo Diallo <billodiallonet@gmail.com>
2021-06-18 15:49:52 +0300

    refacter App class

--- src/components/App.js
+++ src/components/App.js
@@ -4,22 +4,13 @@ import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.props = props;
-//     this.state = {
-//       total: null,
-//       next: null,
-//       operation: null,
-//     };
 
 const App = ()  => {
   const [state,setState] = useState([{
     total:null,
     next:null,
     operation:null,
-  }])
+  }]);
 }
 
     this.handleClick = buttonName => {
