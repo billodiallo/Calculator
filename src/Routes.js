0701915eeca20f3d8b2dda2fb7719746b3980344
Billo Diallo <billodiallonet@gmail.com>
2021-06-21 22:43:41 +0300

    Import component(Home,Navbar,Quote)

--- src/Routes.js
+++ src/Routes.js
@@ -1,3 +1,7 @@
 import React from 'react';
-import {BrowserRouter,Switch,Router} from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import App from './components/App';
+import Home from './components/Home';
+import Quote from './components/Quote';
+import Navbar from './components/Navbar';
 
