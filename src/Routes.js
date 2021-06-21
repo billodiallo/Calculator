0b83597a329a044f1b54d642cdd2478f472b4896
Billo Diallo <billodiallonet@gmail.com>
2021-06-21 23:21:03 +0300

    Add Routes

--- src/Routes.js
+++ src/Routes.js
@@ -5,3 +5,17 @@ import Home from './components/Home';
 import Quote from './components/Quote';
 import Navbar from './components/Navbar';
 
+const Routes = () => (
+    <BrowserRouter>
+    <Navbar/>
+    <Switch>
+        <Route path= "/calculator" component={App}></Route>
+        <Route  path = "/quote" component = {Quote}> </Route>
+        <Route path = "/" component = {Home} ></Route>
+    </Switch>
+    </BrowserRouter>
+
+);
+
+export default Routes;
+
