058e1dcaadc4780ed9d9207276f0767a6fed8df0
Billo Diallo <billodiallonet@gmail.com>
2021-06-23 17:17:18 +0300

    Import Package for testing,add ButtonPanel snapshot

---
+++ src/components/__tests__/ButtonPanel.test.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactTestRenderer from 'react-test-renderer';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import ButtonPanel from '../ButtonPanel';
