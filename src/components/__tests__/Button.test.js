6ab5247dbb61ef466256cc27dc50ed2fe4152af0
Billo Diallo <billodiallonet@gmail.com>
2021-06-23 16:29:58 +0300

    Test button component

--- src/components/__tests__/Button.test.js
+++ src/components/__tests__/Button.test.js
@@ -4,4 +4,11 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Button from '../Button';
 
+const handleClick = () => ({ total: '0', next: '0', operation: 'x' });
 
+describe('Button', () => {
+  it('should render the Button component', () => {
+    const tree = ReactTestRenderer.create(<Button handleClick={handleClick} buttonName="0" styling="zero-style" />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
