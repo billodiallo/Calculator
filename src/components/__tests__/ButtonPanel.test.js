c6cea05bf552b67aee56c7a9a316c23a66d3e25c
Billo Diallo <billodiallonet@gmail.com>
2021-06-23 17:39:40 +0300

    test ButtonPanel

--- src/components/__tests__/ButtonPanel.test.js
+++ src/components/__tests__/ButtonPanel.test.js
@@ -1,5 +1,21 @@
+/* eslint-disable no-undef */
 import React from 'react';
 import ReactTestRenderer from 'react-test-renderer';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import ButtonPanel from '../ButtonPanel';
+
+const handleClick = () => ({ total: '0', next: '0', operation: 'x' });
+
+describe('Button Panel', () => {
+  it('should render the Button panel component', () => {
+    const tree = ReactTestRenderer.create(<ButtonPanel clickHandler={handleClick} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should create the Button Panel component', () => {
+    render(<ButtonPanel clickHandler={handleClick} />);
+    const element = screen.getByText('0');
+    expect(element).toBeInTheDocument();
+  });
+});
