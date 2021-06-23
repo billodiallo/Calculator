b52a3e7320820be25bcc06a1a1afa2a674ae0a01
Billo Diallo <billodiallonet@gmail.com>
2021-06-23 16:00:33 +0300

    Install reactTestRenderer,render,screen, testing library, Button

---
+++ src/components/__tests__/Button.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactTestRenderer from 'react-test-renderer';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Button from '../Button';
+
+
